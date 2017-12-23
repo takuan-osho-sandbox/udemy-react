ff98ea502bcf9d9af7d6472993b28269d3ed2d3d
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-12-23 19:13:02 +0900

    Add SearchForm component

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import SearchForm from './SearchForm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  handlePlaceSubmit(place) {
+    console.log(place);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+      </div>
+    );
+  }
+}
+
+export default App;
